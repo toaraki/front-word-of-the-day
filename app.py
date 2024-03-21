2dc7b155f570bcfd29e0ef00fff2648325c2e2da
Toshihiro Araki <toaraki@redhat.com>
2024-03-21 17:04:48 +0900

    update app.py

--- app.py
+++ app.py
@@ -8,8 +8,14 @@ def index():
     # API を呼び出してデータを取得
     api_url = 'http://word-of-the-day-git.openshift-tempo-operator.svc:8081/rolldice'
     response = requests.get(api_url)
-    data = response.json()
-
+    
+    # JSON データの取得に成功した場合
+    if response.status_code == 200:
+        data = response.json()
+    else:
+        # エラーが発生した場合は空のデータを使用する
+        data = {}
+    
     # 取得したデータをテンプレートに渡してレンダリング
     return render_template('index.html', data=data)
 
